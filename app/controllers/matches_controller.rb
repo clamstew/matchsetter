a73cd99d162cbfffbc1f9006bad9c5858962bdb4
Clay Stewart <claytonmstewart@gmail.com>
2013-10-20 10:37:53 -0500

    Add ability to challenge a user just from the locations page

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,5 +1,6 @@
 class MatchesController < ApplicationController
   before_action :set_match, only: [:show, :edit, :update, :destroy]
+  before_action :set_ntrp_vars, only: [:new, :new_solo]
 
   # GET /matches
   # GET /matches.json
@@ -14,11 +15,21 @@ class MatchesController < ApplicationController
 
   # GET /matches/new
   def new
-    @ntrp_floor = (current_user.skillset - 0.5) if current_user.skillset
-    @ntrp_ceiling = (current_user.skillset + 0.5) if current_user.skillset
+    # @ntrp_floor = (current_user.skillset - 0.5) if current_user.skillset
+    # @ntrp_ceiling = (current_user.skillset + 0.5) if current_user.skillset
     @users  = User.where("skillset >= ? AND skillset <= ?", @ntrp_floor, @ntrp_ceiling) # for finding users
     @match  = Match.new
     @match.players.build
+  end
+
+  # used when clicing on the create match button for a specific user
+  # such as on the location page that lists users
+  def new_solo
+    # @ntrp_floor = (current_user.skillset - 0.5) if current_user.skillset
+    # @ntrp_ceiling = (current_user.skillset + 0.5) if current_user.skillset
+    @user   = User.find(params[:id])
+    @match  = Match.new
+    # @match.players.build
   end
 
   # GET /matches/1/edit
@@ -32,6 +43,29 @@ class MatchesController < ApplicationController
 
     if @match.save
       Player.create(user_id: current_user.id, match_id: @match.id)
+    end
+
+    respond_to do |format|
+      if @match
+        format.html { redirect_to @match, notice: 'Match was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @match }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @match.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # used when clicing on the create match button for a specific user
+  # such as on the location page that lists users
+  def create_solo
+    @match = Match.new(match_params)
+
+    if @match.save
+      # me
+      Player.create(user_id: current_user.id, match_id: @match.id)
+      # opponent
+      Player.create(user_id: params[:id], match_id: @match.id)
     end
 
     respond_to do |format|
@@ -73,6 +107,11 @@ class MatchesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_match
       @match = Match.find(params[:id])
+    end
+
+    def set_ntrp_vars
+      @ntrp_floor = (current_user.skillset - 0.5) if current_user.skillset
+      @ntrp_ceiling = (current_user.skillset + 0.5) if current_user.skillset
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Matchsetter::Application.routes.draw do
+  # NOTE: players probably doesn't need to be here 
+  # the matches model will accept the POST for players
   resources :matches do
     resources :players, :only => [:create, :index, :new]
   end
@@ -7,7 +9,14 @@ Matchsetter::Application.routes.draw do
 
   resources :locations
 
+  # for making new matches from clicking create match with a specific user
+  get '/users/:id/matches/new', to: 'matches#new_solo', as: 'new_user_match'
+  post '/users/:id/matches', to: 'matches#create_solo', as: 'user_matches'
+
+  # root
   root :to => "home#index"
+
+  # user stuff with devise
   devise_for :users, :controllers => {:registrations => "registrations"}
-  resources :users
+  resources :users 
 end
