8b98fe1aaaaa7e3a67f9309e49908d3fa1ef2b82
Clay Stewart <claytonmstewart@gmail.com>
2013-10-20 02:30:01 -0500

    generate player model--it is a join table!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131020072521) do
+ActiveRecord::Schema.define(version: 20131020072911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,16 @@ ActiveRecord::Schema.define(version: 20131020072521) do
   end
 
   add_index "matches", ["court_id"], name: "index_matches_on_court_id", using: :btree
+
+  create_table "players", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "match_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "players", ["match_id"], name: "index_players_on_match_id", using: :btree
+  add_index "players", ["user_id"], name: "index_players_on_user_id", using: :btree
 
   create_table "roles", force: true do |t|
     t.string   "name"
