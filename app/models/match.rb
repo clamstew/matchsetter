e324fe388951120f8292f00c676e6de95854b2ed
Clay Stewart <claytonmstewart@gmail.com>
2013-10-23 14:20:44 -0500

    Add validation if not greater than 4 players. The UI will only allow you to choose 2 at this point, but the server could handle doubles matches at this point

--- app/models/match.rb
+++ app/models/match.rb
@@ -5,13 +5,15 @@ class Match < ActiveRecord::Base
 
   validate :validate_match_players_count
 
-  accepts_nested_attributes_for :players#, :reject_if => lambda { |a| a[:user_id].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :players
 
   private
 
   def validate_match_players_count
     if self.players.length < 2
       errors.add(:match_players,'match created with no opponents')
+    elsif self.players.length > 4
+      errors.add(:match_players,'match created with too many opponents')
     end
   end
 end
